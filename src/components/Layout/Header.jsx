bb78650735770b5a78aeabdced01c27a564d68f2
Gabriel Silva <j.gabriel.888@gmail.com>
2021-06-14 15:19:18 -0400

    fix typo className

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -4,11 +4,11 @@ import mealsImage from '../../assets/meals.jpg';
 const Header = (props) => {
 	return (
 		<>
-			<header classes={classes.header}>
+			<header className={classes.header}>
 				<h1>ReactMeals</h1>
 				<button>Cart</button>
 			</header>
-			<div classes={classes['main-image']}>
+			<div className={classes['main-image']}>
 				<img src={mealsImage} alt="A table full of food!" />
 			</div>
 		</>
