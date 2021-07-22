c6f591ce77129bb58ad7e23c0f4d15cd9fd17a3b
Gabriel Silva <j.gabriel.888@gmail.com>
2021-07-22 14:21:23 -0400

    add animation to cart when item added

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,16 +1,34 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-contex';
 import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+	const [btnIsHighlited, setBtnIsHighlited] = useState(false);
 	const cartCtx = useContext(CartContext);
-	const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+	const { items } = cartCtx;
+	const numberOfCartItems = items.reduce((curNumber, item) => {
 		return curNumber + item.amount;
 	}, 0);
 
+	const btnClasses = `${classes.button} ${btnIsHighlited ? classes.bump : ''}`;
+	useEffect(() => {
+		if (cartCtx.items.length === 0) {
+			return;
+		}
+		setBtnIsHighlited(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlited(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
+
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
