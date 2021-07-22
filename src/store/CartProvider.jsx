9d3f0f91c15ae3b14a4b9efd33e7b6512dce0842
Gabriel Silva <j.gabriel.888@gmail.com>
2021-07-22 14:10:49 -0400

    remove items from cart logic

--- src/store/CartProvider.jsx
+++ src/store/CartProvider.jsx
@@ -11,21 +11,42 @@ const cartReducer = (state, action) => {
 		const updatedTotalAmount =
 			state.totalAmount + action.item.price * action.item.amount;
 
-		const existingCartIndex = state.items.findIndex(
+		const existingCartItemIndex = state.items.findIndex(
 			(item) => item.id === action.item.id
 		);
-		const existingCartItem = state.items[existingCartIndex];
+		const existingCartItem = state.items[existingCartItemIndex];
 		let updatedItems;
+
 		if (existingCartItem) {
 			const updatedItem = {
 				...existingCartItem,
 				amount: existingCartItem.amount + action.item.amount,
 			};
 			updatedItems = [...state.items];
-			updatedItems[existingCartIndex] = updatedItem;
+			updatedItems[existingCartItemIndex] = updatedItem;
 		} else {
-			updatedItems = { ...action.item };
 			updatedItems = state.items.concat(action.item);
+		}
+
+		return {
+			items: updatedItems,
+			totalAmount: updatedTotalAmount,
+		};
+	}
+	if (action.type === 'REMOVE') {
+		const existingCartItemIndex = state.items.findIndex(
+			(item) => item.id === action.id
+		);
+
+		const existingItem = state.items[existingCartItemIndex];
+		const updatedTotalAmount = state.totalAmount - existingItem.price;
+		let updatedItems;
+		if (existingItem.amount === 1) {
+			updatedItems = state.items.filter((item) => item.id !== action.id);
+		} else {
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+			updatedItems = [...state.items];
+			updatedItems[existingCartItemIndex] = updatedItem;
 		}
 
 		return {
@@ -46,6 +67,7 @@ const CartProvider = (props) => {
 	const addItemToCartHandler = (item) => {
 		dispatchCartAction({ type: 'ADD', item: item });
 	};
+
 	const removeItemFromCartHandler = (id) => {
 		dispatchCartAction({ type: 'REMOVE', id: id });
 	};
@@ -54,13 +76,13 @@ const CartProvider = (props) => {
 		items: cartState.items,
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
-		removeITem: removeItemFromCartHandler,
+		removeItem: removeItemFromCartHandler,
 	};
+
 	return (
 		<CartContext.Provider value={cartContext}>
 			{props.children}
 		</CartContext.Provider>
 	);
 };
-
 export default CartProvider;
